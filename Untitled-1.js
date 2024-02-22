c5fada815e09f0fd7fa9833ee502d79a1adf1d91
Trevor Spicka <98053964+tspicka12@users.noreply.github.com>
2024-02-21 20:57:23 -0600

    week 3 group time
    just so i didnt forget lol

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,6 +1,6 @@
-fetch("https://api.apispreadsheets.com/data/oVMs24DDA6KykfDC/", {
+fetch("https://api.apispreadsheets.com/data/0j15oTIGmbFdOs55/", {
 	method: "POST",
-	body: JSON.stringify({"data": {"age":"value","last_name":"value","first_name":"value","contact_email":"value","emergency_contact_phone":"value","emergency_contact_last_name":"value","emergency_contact_first_name":"value"}}),
+	body: JSON.stringify({"data": {"age":"value","camp":"value","contact_email":"value","last_name":"value","overnight":"value","first_name":"value","special_accommodations":"value","emergency_contact_phone":"value","emergency_contact_relation":"value","emergency_contact_last_name":"value","emergency_contact_first_name":"value"}}),
 }).then(res =>{
 	if (res.status === 201){
 		// SUCCESS
